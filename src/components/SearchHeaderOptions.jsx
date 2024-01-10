45455d32ad6936d4f21407183dc4a54af982836f
deepanshu2711 <deepanshusaini2711@gmail.com>
2024-01-10 21:10:36 +0530

    [FEAT] complete search header options

--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,17 +1,26 @@
+"use client"
 import Link from "next/link";
 import {AiOutlineSearch} from "react-icons/ai"
 import { CiImageOn } from "react-icons/ci";
+import { usePathname } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+
 
 const SearchHeaderOptions = () => {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const searthTerm = searchParams.get("searchTerm");
+    
+
     return ( 
-        <div className="flex gap-10 items-center w-screen justify-center">
-            <Link href={"/"} className=" flex gap-2 items-center">
+        <div className="flex gap-10 items-center w-screen justify-center mb-2">
+            <Link href={`/search/web?searchTerm=${searthTerm}`} className={` flex gap-2 items-center ${pathname ==="/search/web" ? "text-blue-600 font-medium " :"text-gray-500"}`}>
                 <AiOutlineSearch />
-                <p className="text-gray-500 hover:underline text-sm ">All</p>
+                <p className=" hover:underline text-sm ">All</p>
             </Link>
-            <Link href={"/"} className="flex gap-2 items-center ">
+            <Link href={`/search/image?searchTerm=${searthTerm}`} className={`flex gap-2 items-center ${pathname ==="/search/image" ? "text-blue-600 font-medium " :"text-gray-500"} `}>
                 <CiImageOn />
-                <p className="text-gray-500 text-sm hover:underline">Images</p>
+                <p className=" text-sm hover:underline">Images</p>
             </Link>
         </div>
      );
